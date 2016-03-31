977feb059feeb07f707c8ab175a19f41e1415a60
Adam Samaha <adamsamahaa@gmail.com>
2016-03-31 16:28:10 -0700

    created js game logic

---
+++ app/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var wideArrColors = [];
+    var canvas = $('div.container');
+    var theAnswer = '';
+    var rainbowSize = 5;
+    var score = 0;
+    var hints = false;
+    var numWrong = 0;
+
+    // run color game by calling my builder functions!
+    makeAndHideDivs();
+    buildNewRound();
+
+
+    // game logic
+    function buildNewRound() {
+        //turn hints back off
+        hints = false;
+
+        // maybe player needs a hint
+        if (numWrong > 3) {
+            $('#toggleHints').addClass('sadGlow');
+        } else {
+            $('#toggleHints').removeClass('sadGlow');
+
+        }
+
+        }
+});
