56f23c889301f1d5a3bb3129b0810d3bc4eb3ade
Adam Samaha <adamsamahaa@gmail.com>
2016-03-31 19:04:07 -0700

    adds an average brightness div

--- app/custom.js
+++ app/custom.js
@@ -90,5 +90,27 @@ $(document).ready(function() {
         if (score) {
             $('#scoreDisplay').text(score);
         }
+
+        // this adds an "average" brightness div that might help the player
+        // Based on color theory of perceived brightness based on human perception of different colors:
+        // http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+        // lum = 0.33*R + 0.5*G + 0.168B 
+        var lum2 = Math.floor((Number(ACArr[0]) * .33 + Number(ACArr[1]) * .5 + Number(ACArr[2]) * .16));
+        var lumGray2 = 'rgba(' + lum2 + ', ' + lum2 + ', ' + lum2 + ', 1)';
+
+        var lumDiv2 = $('<div>' + "the right color is about this dark&nbsp" + '</div>');
+
+        lumDiv2.css('background-color', lumGray2)
+        lumDiv2.attr('id', 'lum2');
+        lumDiv2.addClass('lum');
+        lumDiv2.appendTo(canvas);
+
+        // Assume user starts the game not wanting hints, so hide the clues.
+        if (!hints) {
+            $('.barChart').css('opacity', 0);
+            $('#lum2').css('opacity', 0)
+        }
+    } // end buildNewRound()
+
 });
 
